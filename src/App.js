f008bb7bf7ecc4c7543c09563b5c8d44a91d851e
samplesn08 <samplesn08@gmail.com>
2021-02-18 15:28:29 -0500

    added styled components!

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import "./App.css";
 import Main from './Main'
+import styled from 'styled-components'
+import image from './logo192.png'
+
+const Splash = styled.div`
+  opacity: 1;
+  transition-property: opacity;
+  transition: 1s ease-out;
+  text-align: center;
+`;
+
+const Scroll = styled.h3`
+  padding: 10px;
+  font-size: 1.5rem;
+`;
 
 function App() {
   const dataUrl = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'; //https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
@@ -17,9 +31,12 @@ function App() {
  
   return (
     <div className="App">
-      <div className="App-header">
+      
+      <Splash className="App-header">
+        <img className="App-logo" src={image}></img>
         <h1>Nasa Photo of the Day!</h1>
-      </div>
+        <Scroll>v v v  Scroll down for today's NASA photo! v v v</Scroll>
+      </Splash>
       <div className="main">
         <Main data={data}/>
       </div>
--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,33 @@
 import React from "react";
 import "./App.css";
+import styled from 'styled-components';
+
+const Content = styled.div`
+  position: center;
+  transition: 1s ease-in-out;
+  &:hover {
+    color: white;
+  }
+`;
+
+const PhotoTitle = styled.h2`
+  font-size: 2rem;
+`;
+
+const Info = styled.p`
+  margin: 20px 90px;
+`;
 
 function Main({ data }) {
     return (
-        <div className="main">
-        <h2>{data.title}</h2>
+        <Content className="main">
+        <PhotoTitle>{data.title}</PhotoTitle>
         <div className="image">
           <img src={data.url} alt="randomized from nasa database"></img>
         </div>
-        <p>{data.explanation}</p>
-        <p>Photo copyright: {data.copyright}</p>
-      </div>
+        <Info>{data.explanation}</Info>
+        <p style={{color: 'white'}}>Photo copyright: {data.copyright}</p>
+      </Content>
     )
 }
 
